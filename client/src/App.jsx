19ca18b955d5fe69819db94dbeb392656ba6f72a
aryansinghss321 <87221922+aryansinghss321@users.noreply.github.com>
2026-05-23 12:52:36 +0530

    chore: initial client setup (vite + tailwind)

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working</h1>
+    </div>
+  );
+}
+
+
